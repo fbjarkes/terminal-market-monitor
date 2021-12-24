d606da9a36b211636477d940f9120f6fd87f62ba
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2021-12-24 18:27:17 +0100

    test

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,9 +1,10 @@
 import chai, { expect } from 'chai';
 import { runApp } from './app';
+import fs from 'fs';
 
 describe('MyApp', () => {
     it('should return Hello World!', async () => {
         const res = await runApp();
-        expect(res).to.equal('Hello World!');
+        expect(res).to.equal('Hello World! xxx');
     });
 });

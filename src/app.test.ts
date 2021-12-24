8e1df6b85a4b0412ff90c21445c8011dd40807a3
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2021-12-24 18:25:24 +0100

    Convert tabs to 4 spaces

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,8 +2,8 @@ import chai, { expect } from 'chai';
 import { runApp } from './app';
 
 describe('MyApp', () => {
-	it('should return Hello World!', async () => {
-		const res = await runApp();
-		expect(res).to.equal('Hello World!');
-	});
+    it('should return Hello World!', async () => {
+        const res = await runApp();
+        expect(res).to.equal('Hello World!');
+    });
 });

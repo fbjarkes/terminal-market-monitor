fff782751ac9fb84f47bad298696102d68d0e19e
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2021-12-24 18:38:55 +0100

    eslint warnings

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,11 +1,9 @@
 import chai, { expect } from 'chai';
 import { runApp } from './app';
-import fs from 'fs';
 
 describe('MyApp', () => {
     it('should return Hello World!', async () => {
         const res = await runApp();
-        const a = 'foo';
-        expect(res).to.equal('Hello World! xxx');
+        expect(res).to.equal('Hello World!');
     });
 });

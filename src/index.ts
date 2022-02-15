bc6a70ddc002ba587ecd02eb39609d49d4097e0b
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2022-02-15 12:46:00 +0100

    WIP: random test data

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,16 @@ import { lightgreen, red, plot, PlotConfig } from 'asciichart';
 import Alpaca from '@alpacahq/alpaca-trade-api';
 
 import { logger } from './utils/logger';
-import { getNewHighs, getNewLows, sleep, getSymbols, downloadSnapshot } from './utils/helper';
+import {
+    getNewHighs,
+    getNewLows,
+    sleep,
+    getSymbols,
+    downloadSnapshot,
+    SnapshotMapping,
+    countNewLows,
+    countNewHighs,
+} from './utils/helper';
 
 const MAX_LEN = 100; // TODO: if terminal width is less than 100, use that instead
 const MAX_SYMBOL_PARAMS = 500; // URL query parameter max length is 2048
@@ -25,19 +34,44 @@ const argParser = yargs(process.argv.slice(2)).options({
         usePolygon: false,
     });
 
+    const config: PlotConfig = { offset: 2, colors: [lightgreen, red] };
+
+    let prevSnapshots;
+    const highs: number[] = [];
+    const lows: number[] = [];
+
     while (1) {
         console.log('==========================================================');
         logger.info(`Downloading snapshot for ${symbols.length} symbols`);
         // 1. download snapshots
-        const snapshots = await downloadSnapshot(symbols, alpaca);
-        console.log('AAPL High:', snapshots['AAPL'].High);
-        console.log('TSLA High:', snapshots['TSLA'].High);
-        // 2. if prevSnapshots is not empty, then compare with new snapshots
-        // 3. Count number of new highs and lows
-        // 4. prevSnapshots = currSnapshots
-        // 5. push new highs/lows percentage to h/l array
-        // 7. plot h/l arrays
-        // 8. sleep
+        try {
+            const snapshots = await downloadSnapshot(symbols, alpaca);
+            // 2. if prevSnapshots is not empty, then compare with new snapshots
+            if (prevSnapshots) {
+                // 3. Count number of new highs and lows
+                const lowPercent = (countNewLows(prevSnapshots, snapshots) / 10) * 100;
+                const highPercent = (countNewHighs(prevSnapshots, snapshots) / 10) * 100;
+
+                // 4. prevSnapshots = currSnapshots
+                prevSnapshots = snapshots;
+
+                // 5. push new highs/lows percentage to h/l array
+                if (highs.length === MAX_LEN) {
+                    highs.shift();
+                    lows.shift();
+                }
+                highs.push(highPercent);
+                lows.push(lowPercent);
+
+                // 7. plot h/l arrays                ;
+                console.clear();
+                console.log(plot([highs, lows], config));
+            } else {
+                prevSnapshots = snapshots;
+            }
+        } catch (e) {
+            logger.error(e);
+        }
         await sleep(1000);
     }
 })();
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,8 +2,14 @@ import Alpaca from '@alpacahq/alpaca-trade-api';
 import fs from 'fs/promises';
 import { random } from 'lodash';
 
+export type Snapshot = {
+    High: number;
+    Low: number;
+    Close: number;
+};
+
 export type SnapshotMapping = {
-    [key: string]: any;
+    [key: string]: Snapshot;
 };
 
 export const getSymbols = async (
@@ -21,21 +27,6 @@ export const getSymbols = async (
     return [];
 };
 
-const transformSnapshot = (s: any) => {
-    const bar = s.DailyBar;
-    return {
-        symbol: s.symbol,
-        data: {
-            DateTime: bar.Timestamp,
-            Open: bar.OpenPrice,
-            High: bar.HighPrice,
-            Low: bar.LowPrice,
-            Close: bar.ClosePrice,
-            Volume: bar.Volume,
-        },
-    };
-};
-
 export const downloadSnapshot = async (symbols: string[], alpaca: Alpaca): Promise<SnapshotMapping> => {
     //const snapshots = await alpaca.getSnapshots(symbols);
     const snapshots = [
@@ -43,31 +34,54 @@ export const downloadSnapshot = async (symbols: string[], alpaca: Alpaca): Promi
             symbol: 'AAPL',
             DailyBar: {
                 Timestamp: '2020-01-01T00:00:00.000Z',
-                OpenPrice: '100.00',
-                HighPrice: '100.00',
-                LowPrice: '100.00',
-                ClosePrice: '100.00',
-                Volume: '100',
+                OpenPrice: 100.0,
+                HighPrice: 100.0 + (Math.random() * 10 < 5 ? 0 : 10),
+                LowPrice: 100.0 + (Math.random() * 10 < 5 ? 0 : 10),
+                ClosePrice: 100.0,
+                Volume: 100,
             },
         },
         {
             symbol: 'TSLA',
             DailyBar: {
                 Timestamp: '2020-01-01T00:00:00.000Z',
-                OpenPrice: '100.00',
-                HighPrice: '100.00',
-                LowPrice: '100.00',
-                ClosePrice: '100.00',
-                Volume: '100',
+                OpenPrice: 100.0,
+                HighPrice: 100.0 + (Math.random() * 10 < 5 ? 0 : 10),
+                LowPrice: 100.0 + (Math.random() * 10 < 5 ? 0 : 10),
+                ClosePrice: 100.0,
+                Volume: 100,
             },
         },
     ];
     const res: SnapshotMapping = {};
-    //return snapshots.map(transformSnapshot);
-    snapshots.map(transformSnapshot).forEach((s) => {
-        res[s.symbol] = s.data;
+    snapshots.forEach((s) => {
+        res[s.symbol] = {
+            High: s.DailyBar.HighPrice,
+            Low: s.DailyBar.LowPrice,
+            Close: s.DailyBar.ClosePrice,
+        };
     });
     return res;
+};
+
+export const countNewLows = (prev: SnapshotMapping, curr: SnapshotMapping): number => {
+    let count = 0;
+    for (const symbol in curr) {
+        if (prev[symbol] && prev[symbol].Low > curr[symbol].Low) {
+            count++;
+        }
+    }
+    return count as number;
+};
+
+export const countNewHighs = (prev: SnapshotMapping, curr: SnapshotMapping): number => {
+    let count = 0;
+    for (const symbol in curr) {
+        if (prev[symbol] && prev[symbol].High < curr[symbol].High) {
+            count++;
+        }
+    }
+    return count as number;
 };
 
 // eslint-disable-next-line no-undef
